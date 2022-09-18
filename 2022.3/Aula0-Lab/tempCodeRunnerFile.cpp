d888a6696100d9bf73c2f3d0f57ace59dbbb43dd
gust_rib <gu_crv1@hotmail.com>
2022-09-18 14:06:44 -0300

    atualizando

---
+++ 2022.3/Aula0-Lab/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+Para ler uma linha inteira, e preciso usar a funcao
+// getline(cin,str)
+
+// exemplo:
+
+// string str;
+// getline(cin,str);
+
+// cout << "A string digitada e: " << str;
+// cout << endl;
